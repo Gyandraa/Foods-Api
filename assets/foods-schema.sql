5ce688269a6c941a0dff0ac2ae31d86ac876b82e
Gyandraa <gyandranaupal14@gmail.com>
2024-06-02 21:37:26 +0700

    update schema

--- assets/foods-schema.sql
+++ assets/foods-schema.sql
@@ -2,19 +2,26 @@ CREATE TABLE "foods" (
   "id" integer PRIMARY KEY,
   "name" varchar(100),
   "origin" varchar(100),
-  "ingredient" varchar(100),
+  "ingredient" ingredient[],
   "price" float,
   "created_at" datetime,
   "updated_at" datetime
 );
 
-CREATE TABLE "description" (
+CREATE TABLE "ingredient" (
   "id" integer PRIMARY KEY,
-  "food_id" int,
-  "name" varchar(50),
-  "location" varchar(50),
+  "foods" food[],
   "created_at" datetime,
   "updated_at" datetime
 );
 
-ALTER TABLE "description" ADD FOREIGN KEY ("food_id") REFERENCES "foods" ("id");
+CREATE TABLE "foods_ingredient" (
+  "foods_id" integer,
+  "ingredient_id" integer,
+  PRIMARY KEY ("foods_id", "ingredient_id")
+);
+
+ALTER TABLE "foods_ingredient" ADD FOREIGN KEY ("foods_id") REFERENCES "foods" ("id");
+
+ALTER TABLE "foods_ingredient" ADD FOREIGN KEY ("ingredient_id") REFERENCES "ingredient" ("id");
+
